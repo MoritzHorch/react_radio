b4f0d10d4b8b07c8e9e47e2b767258bef4a0fe47
Yanamoe <moritz.horch@web.de>
2019-02-09 00:01:45 +0100

    Basic layouting
    In addition: paceholder for example radio stations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Header, Grid, Button, Icon, Divider, Segment, Input, List } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      streams: [
+        {
+          title: 'BBC Radio One',
+          url: 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p',
+          active: true
+        }, {
+          title: 'BBC Radio Two',
+          url: 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p',
+          active: false
+        }, {
+          title: 'BBC Radio Three',
+          url: 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p',
+          active: false
+        }
+      ],
+    playing: false };
+  }
+
+  componentDidMount = () => {
+    //TODO: Get previously added streams from local storage
+  };
+
+  renderStreams = () => {
+    
+    
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+
+        <Header as='h1' >
+          <Header.Content className='App-header'>React Radio</Header.Content>
+        </Header>
+
+        { /* Player */ }
+        <Segment basic>
+          <Grid>
+            <Grid.Row>
+              You are listenting to NAME_HERE!
+              <audio autoPlay>
+                <source src="http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p"/>
+              </audio>
+            </Grid.Row>
+            <Grid.Row columns={3}>
+              { this.state.playing ? (
+                <Button icon labelPosition='left'>
+                  <Icon name='pause' /> 
+                  Pause
+                </Button>
+              ) : (
+                <Button icon labelPosition='left'>
+                  <Icon name='play' />
+                  Play
+                </Button>
+              )}
+            </Grid.Row>
+          </Grid>
+        </Segment>
+        
+        { /* Streams */}
+        <Segment basic>
+          <Divider horizontal>Your streams</Divider>
+          <Input icon={{ name: 'add', circular: false, link: true }} placeholder='Add a stream...' />
+          <List divided>
+            {this.state.streams.map((entry, id) => (
+              <List.Item key={id}>
+                <List.Content>
+                  <List.Header>{entry.title}</List.Header>
+                  <List.Description>{entry.url}</List.Description>
+                </List.Content>
+              </List.Item>
+            ))}
+          </List>
+        </Segment>
+
       </div>
     );
   }
