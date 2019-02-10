a3183eb5baa2b93063afab0de967b5b4a3922779
Yanamoe <moritz.horch@web.de>
2019-02-10 12:02:17 +0100

    Added proper grid
    View now depends on the screen size

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Grid, Button, Icon, Divider, Segment, Input, List } from 'semantic-ui-react';
+import { Header, Grid, Button, Icon, Divider, List, Input, GridColumn } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.playerRef = React.createRef();
-    this.state = { 
+    this.state = {
       streams: [
         {
           title: 'BBC Radio One',
@@ -20,9 +20,10 @@ class App extends Component {
           url: 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p',
         }
       ],
-    playing: false,
-    active: null,
-    loading: false };
+      playing: false,
+      active: null,
+      loading: false
+    };
   }
 
   componentDidMount = () => {
@@ -42,71 +43,97 @@ class App extends Component {
 
   handleStreamRemove = (id) => {
     if (id === this.state.active) {  //Check if the stream to delete is currently playing
-      this.setState({active: null});
+      this.setState({ active: null });
     }
     const cpyStreams = [...this.state.streams];
     cpyStreams.splice(id, 1)
-    this.setState({ streams: cpyStreams});
+    this.setState({ streams: cpyStreams });
   }
 
   render() {
     return (
       <div className='App'>
+        <Grid centered>
+          <Grid.Row textAlign='center'>
+            <Grid.Column mobile={16} tablet={12} computer={12}>
+              <Header as='h1' className='App-header'>
+                <Header.Content >React Radio</Header.Content>
+              </Header>
+            </Grid.Column>
+          </Grid.Row>
 
-        <Header as='h1' >
-          <Header.Content className='App-header'>React Radio</Header.Content>
-        </Header>
-
-        { /* Player */ }
-        <Segment basic>
-          <Grid>
-            <Grid.Row>          
-              { this.state.active != null ? (
+          { /* Player */}
+          <Grid.Row >
+            <Grid.Column textAlign='center' mobile={16} tablet={12} computer={12}>
+              {this.state.active != null ? (
                 <audio ref={this.playerRef} autoPlay onLoadStart={() => this.setState({ loading: true })} onCanPlay={() => this.setState({ loading: false })}>
                   <source src={this.state.streams[this.state.active].url} />
                 </audio>
-              ):(
-                'No Stream selected yet!'
-              )}
-            </Grid.Row>
-            <Grid.Row columns={3}>
-              { this.state.playing ? (
+              ) : (
+                  'No Stream selected yet!'
+                )}
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <GridColumn textAlign='center' mobile={16} tablet={12} computer={8}>
+              {this.state.playing ? (
                 <Button icon labelPosition='left'>
-                  <Icon name='pause' /> 
+                  <Icon name='pause' />
                   Pause
                 </Button>
               ) : (
-                <Button icon labelPosition='left'>
-                  <Icon name='play' />
-                  Play
-                </Button>
-              )}
-            </Grid.Row>
-          </Grid>
-        </Segment>
-        
-        { /* Streams */}
-        <Segment basic>
-          <Divider horizontal>Your streams</Divider>
-          <Input icon={{ name: 'add', circular: false, link: true }} placeholder='Add a stream...' />
-          <List divided verticalAlign='middle'>
-            {this.state.streams.map((entry, id) => (
-              
-              <List.Item key={id}>
-                <List.Content floated='right'>
-                  <Button icon onClick={() => this.handleStreamPlay(id)}>
+                  <Button icon labelPosition='left'>
                     <Icon name='play' />
-                  </Button>
-                  <Button icon onClick={() => this.handleStreamRemove(id)}>
-                    <Icon name='delete' />
-                  </Button>
-                </List.Content>
-                <List.Header>{entry.title}</List.Header>
-              </List.Item>
-            ))}
-          </List>
-        </Segment>
+                    Play
+                </Button>
+                )}
+            </GridColumn>
+          </Grid.Row>
 
+          { /* Streams */}
+          <Grid.Row>
+            <Grid.Column mobile={16} tablet={12} computer={8}>
+              <Divider horizontal>Your streams</Divider>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column mobile={16} tablet={6} computer={4}>
+              <Input label='Title' fluid placeholder='Example: BBC Radio One' />
+            </Grid.Column>
+            <Grid.Column mobile={16} tablet={6} computer={4}>
+              <Input label='Stream URL' fluid placeholder='Example: http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p' />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column textAlign='center'>
+              <Button icon labelPosition='left'>
+                <Icon name='music' />
+                Add stream
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <GridColumn mobile={16} tablet={12} computer={8}>
+              <List divided verticalAlign='middle'>
+                {this.state.streams.map((entry, id) => (
+
+                  <List.Item key={id}>
+                    <List.Content floated='right'>
+                      <Button icon onClick={() => this.handleStreamPlay(id)}>
+                        <Icon name='play' />
+                      </Button>
+                      <Button icon onClick={() => this.handleStreamRemove(id)}>
+                        <Icon name='delete' />
+                      </Button>
+                    </List.Content>
+                    <List.Header>{entry.title}</List.Header>
+                  </List.Item>
+                ))}
+              </List>
+            </GridColumn>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
